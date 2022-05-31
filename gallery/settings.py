d7b37f214a5d641e41f50176c1df272b5a07912f
Develper-Gitonga <aizensalim52@gmail.com>
2022-05-31 15:20:57 +0300

    make it better

--- gallery/settings.py
+++ gallery/settings.py
@@ -15,7 +15,6 @@ import django_heroku
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
-# from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
