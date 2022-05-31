a48a4ea9893cd866a2f3a32e5956be586d65957e
Develper-Gitonga <aizensalim52@gmail.com>
2022-05-31 15:13:06 +0300

    make it better

--- pictures/urls.py
+++ pictures/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from . import views
-
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -8,9 +7,6 @@ urlpatterns = [
     path('',views.main,name = 'main'),
     path('search/', views.search_results, name='search_results'),
     path('imagedetails/<int:galleria_id>', views.one_image, name='imagedetails'),
-  
-    
-    
 ] 
 
 if settings.DEBUG:
